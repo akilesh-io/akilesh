63d437f7f60771e64be489b9f274b2335b184376
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-05-16 18:35:40 +0530

    fix: :bug: next.config.js & settings.json
    back to normal config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  CaseSensitivePathsPlugin: true,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
