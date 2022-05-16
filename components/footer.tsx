384dacd9bba2892290421ff0a416d340c111c15f
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-05-16 09:52:10 +0530

    fix: :art: footer
    email subscribe UI has been added to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,57 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Container from './container';
 
 export const Footer = () => {
   return (
     <footer className="text-center text-white bg-white">
+      <div>
+        <form action="">
+          <div className="grid md:grid-cols-3 gird-cols-1 gap-4 flex justify-center items-center">
+            <div className="md:ml-auto md:mb-6">
+              <p className="text-slate-800">
+                <strong>Sign up for my newsletter</strong>
+              </p>
+            </div>
+
+            <div className="md:mb-6">
+              <input
+                type="text"
+                className="
+                form-control
+                block
+                w-full
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+              "
+                id="exampleFormControlInput1"
+                placeholder="Email address" />
+            </div>
+
+            <div className="md:mr-auto mb-6">
+              <button type="submit" className="inline-block px-6 py-2 border-2 
+              border-white text-white font-medium text-xs leading-tight 
+              uppercase rounded hover:bg-neutral-700 bg-slate-900
+              focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+
       <div className="container pt-9">
+        <p className="text-slate-800">Drop a Message</p>
         <div className="flex justify-center mb-9">
           <a href="#!" className="mr-9 text-gray-800">
             <svg
@@ -45,15 +92,13 @@ export const Footer = () => {
               aria-hidden="true"
               focusable="false"
               data-prefix="fab"
-              data-icon="google"
-              className="svg-inline--fa fa-google w-3.5"
+              data-icon="mail"
+              className="svg-inline--fa fa-mail w-3.5"
               role="img"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 488 512"
             >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+              <path d="M424 80H88a56.06 56.06 0 00-56 56v240a56.06 56.06 0 0056 56h336a56.06 56.06 0 0056-56V136a56.06 56.06 0 00-56-56zm-14.18 92.63l-144 112a16 16 0 01-19.64 0l-144-112a16 16 0 1119.64-25.26L256 251.73l134.18-104.36a16 16 0 0119.64 25.26z"
               ></path>
             </svg>
           </a>
@@ -71,23 +116,6 @@ export const Footer = () => {
               <path
                 fill="currentColor"
                 d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
-              ></path>
-            </svg>
-          </a>
-          <a href="#!" className="mr-9 text-gray-800">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="linkedin-in"
-              className="svg-inline--fa fa-linkedin-in w-3.5"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
               ></path>
             </svg>
           </a>
@@ -111,8 +139,10 @@ export const Footer = () => {
         </div>
       </div>
       <div className="text-center text-gray-700 p-4 bg-black/0.25">
-        © 2021 Copyright:
-        <a className="text-gray-800" href="https://tailwind-elements.com/">Tailwind Elements</a>
+        This site uses cookies
+        <Link href='/privacy'>
+          <a className="text-cyan-800"> Learn more.</a>
+        </Link>
       </div>
     </footer >
   );
