6a3394215dd25f269a5953e6cb3e561570b6bbc7
Akilesh <53332156+Akilesh2112@users.noreply.github.com>
2022-05-21 06:25:22 +0000

    Testing codespace be editing home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,38 +20,20 @@ const Home: NextPage = () => {
 
         <main className={styles.main}>
           <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
+            Hello I'm <a href="https://akilesh.io">Akilesh !!</a>
           </h1>
-
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
+        <div className={styles.grid}>
+            <a href="https://play.google.com/store/apps/details?id=com.livestorm.control" className={styles.card}>
+              <h2>Livestorm control &rarr;</h2>
+              <p>Control is my take on a external sidebar plugin for livestorm.</p>
             </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </div>
+        <div className={styles.grid}>
+            <a href="https://article-town-a3add.web.app/" className={styles.card}>
+              <h2>Article town &rarr;</h2>
+              <p>Article town is a web page helps you to share your article and follow others article.</p>
             </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/canary/examples"
-              className={styles.card}
-            >
-              <h2>Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
+        </div>
         </main>
 
       </Layout>
