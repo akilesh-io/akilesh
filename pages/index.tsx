8ab8c289a8d7ae6e6c59be246136685f29219731
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-05-21 12:33:53 +0530

    fix: :bug: `'` - bug fix
    `'` had some issues with eslint resolved by replacing it with '&#39;'

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,17 +20,17 @@ const Home: NextPage = () => {
 
         <main className={styles.main}>
           <h1 className={styles.title}>
-            Hello I'm <a href="https://akilesh.io">Akilesh !!</a>
+            Hello I&#39;m <a href="https://akilesh.io">Akilesh !!</a>
           </h1>
         <div className={styles.grid}>
             <a href="https://play.google.com/store/apps/details?id=com.livestorm.control" className={styles.card}>
-              <h2>Livestorm control &rarr;</h2>
+              <h2>Livestorm control&rarr;</h2>
               <p>Control is my take on a external sidebar plugin for livestorm.</p>
             </a>
         </div>
         <div className={styles.grid}>
             <a href="https://article-town-a3add.web.app/" className={styles.card}>
-              <h2>Article town &rarr;</h2>
+              <h2>Article town&rarr;</h2>
               <p>Article town is a web page helps you to share your article and follow others article.</p>
             </a>
         </div>
