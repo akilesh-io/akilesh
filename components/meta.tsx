684a0ec84945b78414b4c41c0efb34e39a4cc007
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-06-10 05:23:59 +0530

    2 plausible

--- components/meta.tsx
+++ components/meta.tsx
@@ -8,6 +8,11 @@ export default function Meta() {
                 data-domain="akilesh.io"
                 src="https://139.59.86.84:8000/js/plausible.js">
             </script>
+            <script
+                defer
+                data-domain="akilesh.io"
+                src="https://plausible.io/js/plausible.js">
+            </script>
         </Head>
     )
 }
