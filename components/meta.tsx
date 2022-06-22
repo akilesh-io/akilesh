ac33b205d9a78fa3fa5b663012b7d51c548759bc
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-06-22 21:22:00 +0530

    fix: :chart_with_upwards_trend: fixing analytics

--- components/meta.tsx
+++ components/meta.tsx
@@ -6,13 +6,10 @@ export default function Meta() {
             <script
                 defer
                 data-domain="akilesh.io"
-                src="https://139.59.86.84:8000/js/plausible.js">
+                src="https://plausible.articl.me/js/plausible.js">
+
             </script>
-            <script
-                defer
-                data-domain="akilesh.io"
-                src="https://plausible.io/js/plausible.js">
-            </script>
+
         </Head>
     )
 }
