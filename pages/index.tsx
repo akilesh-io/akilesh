ee004c0541d14e4b3352c0753040b7a86a178ea8
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-06-25 22:23:11 +0530

    fix: :bug: removed assigned but not used imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
 
-import Container from '@/components/container'
 import Layout from '@/components/Layout';
 
 
