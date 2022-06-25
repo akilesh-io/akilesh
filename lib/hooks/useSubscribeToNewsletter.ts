ca871b7e88e22325ff4e33bac1269028035f7dbe
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-06-25 22:08:39 +0530

    feat: :sparkles: Newsletter
    Newsletter integrated with revue

---
+++ lib/hooks/useSubscribeToNewsletter.ts
@@ -0,0 +1,41 @@
+import { Form, FormState } from '../types';
+import { useRef, useState } from 'react';
+
+export function useSubscribeToNewsletter() {
+  const [form, setForm] = useState<FormState>({ state: Form.Initial });
+  const inputEl = useRef(null);
+
+  async function subscribe(e) {
+    e.preventDefault();
+    setForm({ state: Form.Loading });
+
+    const res = await fetch(`/api/subscribe`, {
+      body: JSON.stringify({
+        email: inputEl.current.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    });
+
+    const { error } = await res.json();
+
+    if (error) {
+      setForm({
+        state: Form.Error,
+        message: error
+      });
+      return;
+    }
+
+
+    inputEl.current.value = '';
+    setForm({
+      state: Form.Success,
+      message: `Success! 🎉 You are now subscribed to the newsletter.`
+    });
+  }
+
+  return { subscribe, inputEl, form };
+}
