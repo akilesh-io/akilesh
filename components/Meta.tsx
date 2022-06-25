c552e1c0caa95a444a5a5a6eabc8cf81b49b24c8
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-06-25 22:46:47 +0530

    fix: :art: Componet name format

---
+++ components/Meta.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Meta() {
+    return (
+        <Head>
+            <script
+                defer
+                data-domain="akilesh.io"
+                src="https://plausible.articl.me/js/plausible.js" />
+
+        </Head>
+    )
+}
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
-import { Footer } from './footer';
-import { Navbar } from './navbar';
-import Meta from './meta'
+import { Footer } from './Footer';
+import { Navbar } from './Navbar';
+import Meta from './Meta'
 
 type Props = {
   preview?: boolean
