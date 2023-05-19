cfd5ed0d40b8ccd54ce4c8f18224489bab5e8697
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-05-19 13:41:40 +0530

    fix: :fire: sentry is removed from code

--- pages/api/withSentry.ts
+++ pages/api/withSentry.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import { withSentry } from "@sentry/nextjs";
+
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: "Akilesh" });
 };
 
-export default withSentry(handler);
+export default handler;
