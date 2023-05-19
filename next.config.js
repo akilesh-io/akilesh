9e1c054660e10ba4ac225af6cb7cbadf3aa0a805
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-05-19 13:37:06 +0530

    fix: :heavy_minus_sign: sentry is been removed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { withSentryConfig } = require( '@sentry/nextjs' );
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,16 +8,4 @@ const nextConfig = {
   },
 }
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig( nextConfig, sentryWebpackPluginOptions );
+module.exports = withSentryConfig( nextConfig );
