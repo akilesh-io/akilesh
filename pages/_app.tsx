77ee54d11f5cdaf0f6e1692107370a3ab597e9ec
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-06-16 22:07:40 +0530

    feat: :chart_with_upwards_trend: versel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Meta from "@/components/Meta";
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
       <Meta />
     </>
   );
