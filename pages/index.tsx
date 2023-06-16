bae2cb56a2eff9a2115a539b86347dce9db766e5
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-06-16 16:58:58 +0530

    docs: :boom: updated my work tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
 
-import Layout from '@/components/Layout';
-
+import Layout from "@/components/Layout";
 
 const Home: NextPage = () => {
   return (
@@ -20,24 +19,29 @@ const Home: NextPage = () => {
             Hello I&#39;m <a href="https://akilesh.io">Akilesh !!</a>
           </h1>
           <div className={styles.grid}>
-            <a href="https://play.google.com/store/apps/details?id=com.livestorm.control" className={styles.card}>
+            <a href="https://Filmingo.us/" className={styles.card}>
+              <h2>Filmingo&rarr;</h2>
+              <p>
+                Filmingo is an innovative and immersive movie-watching
+                application designed to bring people together, regardless of
+                their physical location
+              </p>
+            </a>
+
+            <a
+              href="https://play.google.com/store/apps/details?id=com.livestorm.control"
+              className={styles.card}
+            >
               <h2>Livestorm control&rarr;</h2>
-              <p>Control is my take on a external sidebar plugin for livestorm.</p>
+              <p>
+                Control is my take on a external sidebar plugin for livestorm.
+              </p>
             </a>
-
-
-            <a href="https://article-town-a3add.web.app/" className={styles.card}>
-              <h2>Article town&rarr;</h2>
-              <p>Article town is a web page helps you to share your article and follow others article.</p>
-            </a>
-
-
           </div>
         </main>
-
       </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
