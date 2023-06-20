a39a734fe8bd8e931c1b308c70bd2199ce9fb566
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-06-20 14:58:04 +0530

    fix: :ambulance: adding get request in subscribe api route to check if it works in server

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -2,11 +2,18 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+type Data = {
+  name: string
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+
+  res.status(200).json({ name: 'Subscribe' })
+
+
   if (req.method === 'POST') {
     const { email } = req.body;
 
