316b452756786d014e80e9fcff5a27b4437cc93f
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-07-03 21:38:03 +0530

    style: :lipstick: Footer updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import type { AppProps } from "next/app";
 import Meta from "@/components/Meta";
 import { Analytics } from '@vercel/analytics/react';
 
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-tomorrow.css'
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
