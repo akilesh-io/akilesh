b9447e6d1a1c8221b3668a339250fe77008d8165
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-07-10 23:42:06 +0530

    fix: :bug: nav bar hover removed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 
-
 export const Navbar = () => {
   const [active, setActive] = useState(false);
   const { theme, setTheme } = useTheme();
@@ -17,16 +16,22 @@ export const Navbar = () => {
       <div className="container mx-auto max-w-4xl mt-6 px-7">
         <nav className="flex items-center flex-wrap lg:p-0   dark:bg-gradient-to-br dark:from-slate-300 dark:via-cyan-100 dark:to-lime-100 rounded-md ">
           <Link href="/" passHref>
-            <Image  src="https://img.icons8.com/doodle/48/iceberg.pnghttps://img.icons8.com/doodle/48/iceberg.png" alt="logo" width={48} height={48} className="p-1" />
+            <Image
+              src="https://img.icons8.com/doodle/48/iceberg.pnghttps://img.icons8.com/doodle/48/iceberg.png"
+              alt="logo"
+              width={48}
+              height={48}
+              className="p-1"
+            />
           </Link>
 
           <div className="group ml-auto">
             <button
-              className="inline-flex p-3 group-hover:bg-cyan-900 rounded lg:hidden text-white hover:text-white outline-none"
+              className="inline-flex p-3 rounded lg:hidden text-white "
               onClick={handleClick}
             >
               <svg
-                className="w-6 h-6 text-cyan-900 group-hover:text-white"
+                className="w-6 h-6 text-cyan-900 "
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -50,25 +55,25 @@ export const Navbar = () => {
             <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto pt-6 lg:pt-0 lg:mr-5">
               <Link
                 href="/blog"
-                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center hover:bg-white"
+                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center "
               >
                 Blog
               </Link>
               <Link
                 href="/work"
                 passHref
-                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center hover:bg-white"
+                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center "
               >
                 Work
               </Link>
               <a
                 href="mailto:2112akilesh@gmail.com"
-                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center hover:bg-white"
+                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center "
               >
                 Contact
               </a>
               <button
-                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center hover:bg-white"
+                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center "
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
               >
                 {theme === "dark" ? (
@@ -94,26 +99,25 @@ export const Navbar = () => {
             <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto pt-6 lg:pt-0 lg:mr-5">
               <Link
                 href="/blog"
-                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center hover:bg-white"
+                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center "
               >
                 Blog
               </Link>
               <Link
                 href="/work"
                 passHref
-                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center hover:bg-white"
+                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center "
               >
                 Work
               </Link>
               <a
                 href="mailto:2112akilesh@gmail.com"
-                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center hover:bg-white"
+                className="lg:inline-flex lg:w-auto lg:mr-5 w-full px-3 py-2 rounded text-gray-700 font-bold items-center justify-center "
               >
                 Contact
               </a>
 
               <button
-              
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
               >
                 {theme === "dark" ? (
