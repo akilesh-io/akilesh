bee73e4235829b1da17ade39d296c8e14d32d7ff
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-07-10 19:29:55 +0530

    style: :wrench: tailwind config for dark theam and blog post typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,47 @@
+const colors = require( 'tailwindcss/colors' );
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.tsx',
+    './lib/**/*.tsx'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    typography: (theme) => ({
+      DEFAULT: {
+        css: {
+          color: theme('colors.gray.700'),
+          p: {
+            color: theme('colors.gray.700'),
+            '&:hover': {
+              color: theme('colors.gray.600'),
+            },
+          },
+        },
+      },
+      
+      dark: {
+        css: {
+          color: theme('colors.gray.300'),
+          p: {
+            color: theme('colors.gray.100'),
+            '&:hover': {
+              color: theme('colors.gray.200'),
+            },
+          },
+        },
+      },
+
+    }),
+    extend: {
+      fontFamily: {
+        openSans: [ 'var(--font-open-sans)' ],
+        ptSerif: [ 'var(--font-pt-serif)' ],
+        fancy: ['Sriracha']
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/typography')]
+};
