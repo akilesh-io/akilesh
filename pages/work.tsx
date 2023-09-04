b8777dbf6b90dc2b6953ed9f9882082d3aea4d7a
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-09-04 20:58:40 +0530

    work page updated

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import Layout from "layout/Layout";
-import styles from "@/styles/Home.module.css";
 import Head from "next/head";
-import { title } from "process";
 import Card from "@/components/Card";
 
 export default function Work() {
@@ -13,29 +11,6 @@ export default function Work() {
         <meta name="description" content="Works" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      {/* <div className={styles.main}>
-        <div className={styles.grid}>
-          <a
-            href="https://play.google.com/store/apps/details?id=com.livestorm.control"
-            className={styles.card}
-          >
-            <h2 className="text-gray-900 dark:text-white">
-              Livestorm control&rarr;
-            </h2>
-            <p>
-              Control is my take on a external sidebar plugin for livestorm.
-            </p>
-          </a>
-
-          <a href="https://filmingo.us/" className={styles.card}>
-            <h2>Filmingo&rarr;</h2>
-            <p>
-              Filmingo is an innovative and immersive movie-watching
-              application.
-            </p>
-          </a>
-        </div>
-      </div> */}
 
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
         {works.map(({ title, description, link, image }) => (
@@ -58,7 +33,8 @@ const works = [
     description:
       "Control is my take on a external sidebar plugin for livestorm.",
     link: "https://play.google.com/store/apps/details?id=com.livestorm.control",
-    image: "https://res.cloudinary.com/davkfrmah/image/upload/v1693814513/livestorm%20logo.png",
+    image:
+      "https://res.cloudinary.com/davkfrmah/image/upload/v1693814513/livestorm%20logo.png",
   },
   {
     title: "Filmingo",
@@ -67,5 +43,19 @@ const works = [
     link: "https://filmingo.us/",
     image:
       "https://res.cloudinary.com/davkfrmah/image/upload/v1686146984/Filmingo/temp_film_bvuced.jpg",
+  },
+  {
+    title: "2050 Technologies",
+    description: " Green & Eco-Friendly Theme Landing Page ",
+    link: "https://www.2050-technologies.com/",
+    image:
+      "https://res.cloudinary.com/davkfrmah/image/upload/c_scale,q_75,w_659/v1693820228/Akilesh/2050%20Technologies.jpg",
+  },
+  {
+    title: "The Boss Gym",
+    description: "Landing Page based on Gym and Fitness ",
+    link: "https://www.thebossgym.in/",
+    image:
+      "https://res.cloudinary.com/davkfrmah/image/upload/v1693840960/Akilesh/the%20boss%20gym.jpg",
   },
 ];
