d0aaaadba3eb54103095dfee60fb4183aa0dc6d6
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-09-04 15:11:08 +0530

    feat: :memo: work list updated

---
+++ components/Card.tsx
@@ -0,0 +1,39 @@
+import Balancer from "react-wrap-balancer";
+import Image from "next/image";
+export default function Card({
+  title,
+  description,
+  link,
+  image,
+}: {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+}) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      className="relative col-span-1 h-96 overflow-hidden rounded-xl bg-white shadow-md "
+    >
+      <div className="flex h-60 items-center justify-center">
+        <Image
+          width={400}
+          height={400}
+          src={image}
+          alt={title}
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      <div className="mx-auto max-w-md text-center mt-1">
+        <h2 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text font-display text-xl font-bold text-transparent md:text-3xl md:font-normal">
+          <Balancer>{title}</Balancer>
+        </h2>
+        <div className="prose-sm mt-2 leading-normal text-gray-500 md:prose">
+          {description}
+        </div>
+      </div>
+    </a>
+  );
+}
--- pages/work.tsx
+++ pages/work.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Layout from "layout/Layout";
 import styles from "@/styles/Home.module.css";
 import Head from "next/head";
+import { title } from "process";
+import Card from "@/components/Card";
 
 export default function Work() {
   return (
@@ -11,13 +13,15 @@ export default function Work() {
         <meta name="description" content="Works" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className={styles.main}>
+      {/* <div className={styles.main}>
         <div className={styles.grid}>
           <a
             href="https://play.google.com/store/apps/details?id=com.livestorm.control"
             className={styles.card}
           >
-            <h2 className="text-gray-900 dark:text-white">Livestorm control&rarr;</h2>
+            <h2 className="text-gray-900 dark:text-white">
+              Livestorm control&rarr;
+            </h2>
             <p>
               Control is my take on a external sidebar plugin for livestorm.
             </p>
@@ -31,7 +35,37 @@ export default function Work() {
             </p>
           </a>
         </div>
+      </div> */}
+
+      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
+        {works.map(({ title, description, link, image }) => (
+          <Card
+            key={title}
+            title={title}
+            description={description}
+            link={link}
+            image={image}
+          />
+        ))}
       </div>
     </Layout>
   );
 }
+
+const works = [
+  {
+    title: "Livestorm control",
+    description:
+      "Control is my take on a external sidebar plugin for livestorm.",
+    link: "https://play.google.com/store/apps/details?id=com.livestorm.control",
+    image: "https://res.cloudinary.com/davkfrmah/image/upload/v1693814513/livestorm%20logo.png",
+  },
+  {
+    title: "Filmingo",
+    description:
+      "Filmingo is an innovative and immersive movie-watching application.",
+    link: "https://filmingo.us/",
+    image:
+      "https://res.cloudinary.com/davkfrmah/image/upload/v1686146984/Filmingo/temp_film_bvuced.jpg",
+  },
+];
