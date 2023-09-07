acd9c370dd1434cc0e37f555f49e206eef756fac
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-09-07 18:12:07 +0530

    work page ui update

--- pages/work.tsx
+++ pages/work.tsx
@@ -12,9 +12,14 @@ export default function Work() {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <span className="block text-base font-semibold tracking-wide text-center text-slate-800 uppercase dark:text-sky-400 mt-8">
-        My Work
-      </span>
+      <h1>
+        <span className="block text-base font-semibold tracking-wide text-center text-slate-800 uppercase dark:text-sky-400 mt-8">
+          My Work
+        </span>
+        <span className="block max-w-2xl mx-auto mt-2 text-4xl font-bold leading-10 text-center sm:text-5xl m-4">
+          Some of My Recent Projects
+        </span>
+      </h1>
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
         {works.map(({ title, description, link, image }) => (
           <Card
@@ -49,12 +54,18 @@ export default function Work() {
 
 const works = [
   {
-    title: "Livestorm control",
-    description:
-      "Control is my take on a external sidebar plugin for livestorm.",
-    link: "https://play.google.com/store/apps/details?id=com.livestorm.control",
+    title: "The Boss Gym",
+    description: "Landing Page based on Gym and Fitness ",
+    link: "https://www.thebossgym.in/",
     image:
-      "https://res.cloudinary.com/davkfrmah/image/upload/v1693814513/livestorm%20logo.png",
+      "https://res.cloudinary.com/davkfrmah/image/upload/v1693840960/Akilesh/the%20boss%20gym.jpg",
+  },
+  {
+    title: "2050 Technologies",
+    description: " Green & Eco-Friendly Theme Landing Page ",
+    link: "https://www.2050-technologies.com/",
+    image:
+      "https://res.cloudinary.com/davkfrmah/image/upload/c_scale,q_75,w_659/v1693820228/Akilesh/2050%20Technologies.jpg",
   },
   {
     title: "Filmingo",
@@ -65,17 +76,11 @@ const works = [
       "https://res.cloudinary.com/davkfrmah/image/upload/v1686146984/Filmingo/temp_film_bvuced.jpg",
   },
   {
-    title: "2050 Technologies",
-    description: " Green & Eco-Friendly Theme Landing Page ",
-    link: "https://www.2050-technologies.com/",
+    title: "Livestorm control",
+    description:
+      "Control is my take on a external sidebar plugin for livestorm.",
+    link: "https://play.google.com/store/apps/details?id=com.livestorm.control",
     image:
-      "https://res.cloudinary.com/davkfrmah/image/upload/c_scale,q_75,w_659/v1693820228/Akilesh/2050%20Technologies.jpg",
-  },
-  {
-    title: "The Boss Gym",
-    description: "Landing Page based on Gym and Fitness ",
-    link: "https://www.thebossgym.in/",
-    image:
-      "https://res.cloudinary.com/davkfrmah/image/upload/v1693840960/Akilesh/the%20boss%20gym.jpg",
+      "https://res.cloudinary.com/davkfrmah/image/upload/v1693814513/livestorm%20logo.png",
   },
 ];
