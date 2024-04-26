af4b6f66a710e0f93be12fb503df630219a674c1
akilesh-io <2112akilesh@gmail.com>
2024-04-26 22:43:50 +0530

    feat: :rocket: blog.akilesh.in -> akilesh.in
    Trying to configure Lamento reverse proxy

--- next.config.js
+++ next.config.js
@@ -4,30 +4,34 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [ 
-      's3.us-west-2.amazonaws.com', // Images coming from Notion
-      'www.notion.so', // Images coming from Notion
-      'via.placeholder.com', // for articles that do not have a cover image
-      'pbs.twimg.com', // Twitter Profile Picture
-      'iad.microlink.io', // link preview
-      'images.unsplash.com', // If notion cover image is from, unsplash
-      'res.cloudinary.com',
-      "img.icons8.com"      
+    domains: [
+      "s3.us-west-2.amazonaws.com", // Images coming from Notion
+      "www.notion.so", // Images coming from Notion
+      "via.placeholder.com", // for articles that do not have a cover image
+      "pbs.twimg.com", // Twitter Profile Picture
+      "iad.microlink.io", // link preview
+      "images.unsplash.com", // If notion cover image is from, unsplash
+      "res.cloudinary.com",
+      "img.icons8.com",
     ],
   },
   rewrites: async () => [
     {
-      source: '/api/:path*',
+      source: "/blog/:subdir*",
+      destination: "https://blog.akilesh.in/:subdir*",
+    },
+    { source: "/(.*)", destination: "/" },
+    {
+      source: "/api/:path*",
       has: [
         {
-          type: 'header',
-          key: 'Access-Control-Allow-Origin',
+          type: "header",
+          key: "Access-Control-Allow-Origin",
         },
       ],
-      destination: 'http://localhost:3000/api/:path*',
+      destination: "http://localhost:3000/api/:path*",
     },
   ],
-
-}
+};
 
 module.exports = nextConfig;
