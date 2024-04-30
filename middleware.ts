96d38e1f70e2727c19be3680dc5f96011a3e7787
akilesh-io <2112akilesh@gmail.com>
2024-04-30 18:23:37 +0530

    fix: :fire:

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export default async function middleware(req) {
-  const { pathname } = req.nextUrl
-  if (pathname.startsWith('/blogs')) {
-    return NextResponse.rewrite(new URL(pathname, "https://akilesh.lamento.in/"))
-  }
-  return NextResponse.next()
-}
