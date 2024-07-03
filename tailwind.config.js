777c4ed361f0aaef43eaee9b13fdc05603a70915
akilesh-io <2112akilesh@gmail.com>
2024-07-03 21:30:31 +0530

    feat: :sparkles: Marquee component added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,61 @@
-const colors = require( 'tailwindcss/colors' );
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.tsx',
-    './lib/**/*.tsx'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.tsx",
+    "./lib/**/*.tsx",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     typography: (theme) => ({
       DEFAULT: {
         css: {
-          color: theme('colors.gray.700'),
+          color: theme("colors.gray.700"),
           p: {
-            color: theme('colors.gray.700'),
-            '&:hover': {
-              color: theme('colors.gray.600'),
-            },
-          },
-        },
-      },
-      
-      dark: {
-        css: {
-          color: theme('colors.gray.300'),
-          p: {
-            color: theme('colors.gray.100'),
-            '&:hover': {
-              color: theme('colors.gray.200'),
+            color: theme("colors.gray.700"),
+            "&:hover": {
+              color: theme("colors.gray.600"),
             },
           },
         },
       },
 
+      dark: {
+        css: {
+          color: theme("colors.gray.300"),
+          p: {
+            color: theme("colors.gray.100"),
+            "&:hover": {
+              color: theme("colors.gray.200"),
+            },
+          },
+        },
+      },
     }),
     extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
       fontFamily: {
-        openSans: [ 'var(--font-open-sans)' ],
-        ptSerif: [ 'var(--font-pt-serif)' ],
-        bigilla: [ 'var(--font-bigilla)' ],
-        fancy: ['Sriracha']
+        openSans: ["var(--font-open-sans)"],
+        ptSerif: ["var(--font-pt-serif)"],
+        bigilla: ["var(--font-bigilla)"],
+        fancy: ["Sriracha"],
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [require("@tailwindcss/typography")],
 };
