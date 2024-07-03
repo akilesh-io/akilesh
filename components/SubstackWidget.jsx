59cc1f34f18733dd3759b689c787e15b1756296c
akilesh-io <2112akilesh@gmail.com>
2024-07-03 22:08:59 +0530

    fix: :lipstick:

--- components/SubstackWidget.jsx
+++ components/SubstackWidget.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
+import { useTheme } from "next-themes";
 
 const SubstackWidget = () => {
+  
+  const { theme } = useTheme()
+
+  
   useEffect(() => {
     // Define the CustomSubstackWidget on the window object
     window.CustomSubstackWidget = {
@@ -9,10 +14,11 @@ const SubstackWidget = () => {
       buttonText: "Subscribe",
       theme: "custom",
       colors: {
-        primary: "#FFFFFF",
-        input: "#334155",
-        email: "#FFFFFF",
-        text: "#000000",
+        primary: `${theme === "dark" ? "#e8e5f0" : "#0e1012"}`, //e8e5f0
+        // input: "#0e1012", //e8e5f0
+        input: `${theme === "dark" ? "#0e1012" : "#e8e5f0"}`, //e8e5f0
+        email:  `${theme === "dark" ? "#e8e5f0" : "#0e1012"}`, //e8e5f0
+        text: `${theme === "dark" ? "#0e1012" : "#e8e5f0"}`, //e8e5f0
       },
     };
 
@@ -30,7 +36,7 @@ const SubstackWidget = () => {
     return () => {
       document.body.removeChild(script);
     }
-  }, []);
+  }, [theme]);
 
   return (
     <div id="custom-substack-embed"></div>
