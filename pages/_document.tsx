6e812f2b0f5fc196e35ab87f41359b056275cd64
akilesh-io <2112akilesh@gmail.com>
2024-07-04 23:13:42 +0530

    chore: :mag: mastodon Verifying my identity
    link tag added to header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-
+          <link rel="me" href="https://mastodon.social/@akilesh_io" />
         </Head>
         <body className="prose-headings:font-headings dark:bg-[#0e1012]">
           <div className='bg'></div>
