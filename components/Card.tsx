7a8aee174140baaf75936e30915b346acc7dd01c
akilesh-io <2112akilesh@gmail.com>
2024-07-19 19:39:23 +0530

    fix: :lipstick: work section card text colour updated for dark and light theam

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,7 +12,7 @@ export default function Card({
   image: string;
 }) {
   return (
-    <div className="relative col-span-1 h-96 overflow-hidden rounded-xl bg-white shadow-md ">
+    <div className="relative col-span-1 h-96 overflow-hidden rounded-xl bg-white/30 dark:bg-[#27272a]/70 shadow-md ">
       <a href={link} target="_blank">
         <div className="flex h-60 items-center justify-center">
           <Image
@@ -25,10 +25,10 @@ export default function Card({
           />
         </div>
         <div className="mx-auto max-w-md text-center mt-1">
-          <h2 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text font-display text-xl font-bold text-transparent md:text-3xl md:font-normal">
+          <h2 className="text-gray-800 dark:text-gray-300 bg-clip-text font-display text-xl font-bold md:text-3xl md:font-normal">
             <Balancer>{title}</Balancer>
           </h2>
-          <div className="prose-sm mt-2 leading-normal text-gray-500 md:prose">
+          <div className="text-gray-700 dark:text-gray-400 prose-sm mt-2 leading-normal md:prose">
             {description}
           </div>
         </div>
