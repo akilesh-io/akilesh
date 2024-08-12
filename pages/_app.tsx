4c7f7663b7ae8ef88853364b2187ca517d8260a0
akilesh-io <2112akilesh@gmail.com>
2024-08-12 19:18:59 +0530

    feat: :sparkles: preloading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,14 @@ import localFont from "next/font/local";
 import { PT_Serif, Open_Sans } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
 import MicrosoftClarity from "@/components/MicrosoftClarity";
-
+import Preloader from "../components/Preloader";
+import { PageTransition } from "@/components/PageTransition";
 // core styles shared by all of react-notion-x (required)
 import "react-notion-x/src/styles.css";
 // used for code syntax highlighting (optional)
 import "prismjs/themes/prism-tomorrow.css";
+import { useState, useEffect } from "react";
+import { AnimatePresence } from "framer-motion";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -21,18 +24,35 @@ const ptSerif = PT_Serif({
   subsets: ["latin"],
   variable: "--font-pt-serif",
 });
-const bigilla = localFont({ 
+const bigilla = localFont({
   src: '../public/fonts/Bigilla.otf',
   variable: '--font-bigilla'
- })
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+
   return (
     <>
       <Meta />
       <ThemeProvider attribute="class">
         <main className={`${openSans.variable} ${ptSerif.variable} font-sans ${bigilla.variable}`}>
-          <Component {...pageProps} />
+          <AnimatePresence mode="wait">
+            {isLoading && <Preloader />}
+          </AnimatePresence>
+          <PageTransition>
+            <Component {...pageProps} />
+          </PageTransition>
         </main>
       </ThemeProvider>
 
