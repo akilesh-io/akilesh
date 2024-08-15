4e329ce112d04f5ed5b626d742915141e4ae33a7
akilesh-io <2112akilesh@gmail.com>
2024-08-15 19:24:30 +0530

    style: Update Card component grid layout for better responsiveness

--- components/Card.tsx
+++ components/Card.tsx
@@ -21,7 +21,7 @@ export const HoverEffect = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3",
+        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-8",
         className
       )}
     >
