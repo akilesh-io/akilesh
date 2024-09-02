8a3e39746b3e83c50ba9d45603b47a39cac1994e
akilesh-io <2112akilesh@gmail.com>
2024-09-02 20:32:07 +0530

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import Marquee from "@/components/Marquee";
 import UnderConstruction from "@/components/UnderConstruction";
 // import {MyScene1} from "@/components/Scene/MyScene1";
 // import WoahText from "@/components/Scene/woahText";
-import MyScene from "@/components/Scene/MyScene";
+import ComputerCanvas from "@/components/Scene/ComputerCanvas";
 
 const Home: NextPage = () => {
   return (
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
           <link rel="icon" href="/favicon.png" />
         </Head>
         <div className="w-full h-screen overflow-hidden">
-          <MyScene />
+          <ComputerCanvas />
         </div>
         <Marquee />
         <UnderConstruction />
