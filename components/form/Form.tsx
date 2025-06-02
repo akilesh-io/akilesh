876c71c7f49a5c809b9ffe0a1b43b8eef1f896e0
akilesh-io <2112akilesh@gmail.com>
2025-06-02 22:22:44 +0530

    Start Your Project buttom made small in mobile screen

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -168,13 +168,13 @@ export function Form() {
         <>
             <Modal>
                 <ModalTrigger className="relative inline-block text-lg group">
-                    <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                        <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                    <span className="relative z-10 block px-4 py-2 sm:px-5 sm:py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white text-base sm:text-lg">
+                        <span className="absolute inset-0 w-full h-full px-4 py-2 sm:px-5 sm:py-3 rounded-lg bg-gray-50"></span>
+                        <span className="absolute left-0 w-32 h-32 sm:w-48 sm:h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-8 sm:translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
                         <span className="relative font-semibold">Start Your Project</span>
                     </span>
                     <span
-                        className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                        className="absolute bottom-0 right-0 w-full h-8 sm:h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
                         data-rounded="rounded-lg"
                     ></span>
                 </ModalTrigger>
