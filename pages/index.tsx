12ee6e257804be411ebefb432b8047fa6f361b9a
akilesh-io <2112akilesh@gmail.com>
2025-12-30 23:02:09 +0530

    feat: preloader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import Lottie from "lottie-react";
-import spaceGuy from "@/public/animation/space-guy.json";
-import walkMan from "@/public/animation/walk-man.json";
 import Layout from "layout/Layout";
 import Marquee from "@/components/Marquee";
 import UnderConstruction from "@/components/UnderConstruction";
 // import {MyScene1} from "@/components/Scene/MyScene1";
 // import WoahText from "@/components/Scene/woahText";
 import ComputerCanvas from "@/components/Scene/ComputerCanvas";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -18,7 +14,7 @@ const Home: NextPage = () => {
       <Layout>
         <Head>
           <title>Akilesh</title>
-          <meta name="description" content="akilesh io" />
+          <meta name="description" content="akilesh" />
           <link rel="icon" href="/favicon.png" />
         </Head>
         <div className="w-full h-screen overflow-hidden">
