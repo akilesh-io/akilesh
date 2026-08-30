3367952c6dd82d43cfbce2c536cc4ef579831f6c
akilesh-io <2112akilesh@gmail.com>
2026-08-30 16:30:35 +0530

    perf(motion): use y instead of top in slideUp to avoid layout shift

--- components/Preloader/anim.js
+++ components/Preloader/anim.js
@@ -10,10 +10,10 @@ export const opacity = {
 
 export const slideUp = {
     initial: {
-        top: 0
+        y: 0
     },
     exit: {
-        top: "-100vh",
+        y: "-100vh",
         transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2}
     }
 }
